58e1af24d4312cecbca47221abe010d32cd99a46
matigfv <matigfv@gmail.com>
2026-07-08 23:18:13 +0200

    initial

--- src/main/java/com/api/identity/UserApplication.java
+++ src/main/java/com/api/identity/UserApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserApplication {
-	static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
 }
